25466eaf587029e936623293d98c614d8c6afb03
Roman Zalov <romanzalov@gmail.com>
2022-03-31 02:32:19 -0500

    responsive design

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -4,26 +4,30 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
+import { Grid } from "@mui/material";
 
 const NavBar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" style={{ backgroundColor: "#800080" }}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          ></IconButton>
+          <Grid container>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Purple Team CSC 394
           </Typography>
-          <Button color="inherit">Most Popular Recipes</Button>
-          <Button color="inherit">Vegan Recipes</Button>
-          <Button color="inherit">Random Recipes</Button>
-          <Button color="inherit">Login</Button>
+            <Grid item>
+            <Button color="inherit">Most Popular Recipes</Button>
+            </Grid>
+            <Grid item>
+            <Button color="inherit">Vegan Recipes</Button>
+            </Grid>
+            <Grid item>
+            <Button color="inherit">Random Recipes</Button>
+            </Grid>
+            <Grid item>
+            <Button color="inherit">Login</Button>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     </Box>
